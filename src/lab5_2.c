4e03d89cff7cc9287497d3c24374d0346d83bda4
RinokuS <JeffTheKilller@mail.ru>
2021-02-16 14:19:25 +0300

    Codestyle fixes in lab5

--- src/lab5_2.c
+++ src/lab5_2.c
@@ -26,8 +26,6 @@ int main() {
         // PARENT
         // Закрываем на чтение родителя
         close(parent[0]);
-        // Закрываем на запись ребёнка
-        close(child[1]);
         // Пишем HelloWorld в родителя
         size = write(parent[1], "Hello, world!", 14);
     
@@ -35,9 +33,11 @@ int main() {
             printf("Can\'t write all string\n");
             exit(-1);
         }
-        // Закрываем запись родителя
+        // Закрываем на запись родителя
         close(parent[1]);
 
+        // Закрываем на запись ребёнка
+        close(child[1]);
         // Читаем ребёнка
         size = read(child[0], resstring, 14);
         if (size != 14) {
@@ -51,22 +51,24 @@ int main() {
         // CHILD
         // Закрывем на запись родителя
         close(parent[1]);
-        // Закрываем на чтение ребёнка
-        close(child[0]);
         // Читаем родителя
         size = read(parent[0], resstring, 14);
         if (size != 14) {
             printf("Can\'t read from parent\n");
             exit(-1);
         }
+        // Закрываем на чтение родителя
+        close(parent[0]);
 
         // Печатаем, что прочитали:
         printf("Child read from parent: %s\n", resstring);
+
+        // Закрываем на чтение ребёнка
+        close(child[0]);
         // Пишем, что ребенок думает о мире:
         size = write(child[1], "World is hell", 14);
-        
+        // Закрываем на запись ребёнка
         close(child[1]);
-        close(parent[0]);
         printf("Child is done.\n");
     }
     return 0;
