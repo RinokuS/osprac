9fff16fedf0fad5d89a4dec3d5addc2641683d4b
RinokuS <JeffTheKilller@mail.ru>
2021-03-11 01:34:00 +0300

    Lab 7 fixes

--- src/lab7_1.c
+++ src/lab7_1.c
@@ -7,45 +7,69 @@
 
 
 int main() {
-    int shmid;
+    int shmid_for_size, shmid;
     char pathname[] = "lab7_1.c";
-    key_t key;
-    char *source_code;
+    char pathname_for_size[] = "lab7_2.c";
+    key_t key_for_size, key;
     
     int file_size = 0;
-    int *shared_mem_ptr;
+    int *shared_mem_ptr_size;
+    char *shared_mem_ptr;
     FILE *file;
     file = fopen(pathname, "r");
     
     fseek(file, 0L, SEEK_END);
     file_size = ftell(file); // получаем размер файла
     rewind(file); // перемещаем указатель обратно на начало потока, ибо подсчет размера его двигает
+
+    // генерируем ключ разделяемой памяти для переменной размера файла
+    if ((key_for_size = ftok(pathname_for_size, 0)) < 0) { 
+        printf("Can\'t generate key\n"); 
+        exit(-1);
+    }
     // генерируем ключ разделяемой памяти (позволяет нам не пересекать деятельность независимых процессов)
     if ((key = ftok(pathname, 0)) < 0) { 
         printf("Can\'t generate key\n"); 
         exit(-1);
     }
-    // получаем по ключу дескриптор нужного размера
-    if ((shmid = shmget(key, sizeof(int) + file_size * sizeof(char), 0666|IPC_CREAT)) < 0) {
+
+    // получаем по ключу дескриптор размером в один инт
+    if ((shmid_for_size = shmget(key_for_size, sizeof(int), 0666|IPC_CREAT)) < 0) {
         printf("Can\'t create shared memory\n");
         exit(-1);
     }
+    // получаем по ключу дескриптор нужного размера
+    if ((shmid = shmget(key, file_size * sizeof(char), 0666|IPC_CREAT)) < 0) {
+        printf("Can\'t create shared memory\n");
+        exit(-1);
+    }
+
     // получаем адрес нашей разделяемой памяти для работы с ней
-    if ((shared_mem_ptr = (int *)shmat(shmid, 
+    if ((shared_mem_ptr_size = (int *)shmat(shmid_for_size, 
                                         NULL, // желаемый адрес, куда мы хотим записать нашу разделяемую память, в нашем случае нам все равно
                                         0)) == (int *)(-1)) {
         printf("Can't attach shared memory\n");
         exit(-1);
     }
-    // в первый слот памяти записываем размер файла, для того мы и выделили дополнительные sizeof(int) байт
-    *shared_mem_ptr = file_size;
-    // сначала двигаем наш указатель на один инт, ибо там хранится размер файла, и приводим к чару для верной дальнейшей итерации
-    source_code = (char*)(shared_mem_ptr + 1); 
+    // получаем адрес нашей разделяемой памяти для работы с ней
+    if ((shared_mem_ptr = (char *)shmat(shmid, 
+                                        NULL, // желаемый адрес, куда мы хотим записать нашу разделяемую память, в нашем случае нам все равно
+                                        0)) == (char *)(-1)) {
+        printf("Can't attach shared memory\n");
+        exit(-1);
+    }
+    // записываем размер файла в первый блок разделяемой памяти
+    *shared_mem_ptr_size = file_size;
     // записываем наш сурс код в память
     for (int i = 0; i < file_size; ++i)
-        source_code[i] = fgetc(file); // считываем с помощью fgetc, но можно читать и read`ом "блоками" по sizeof(char) байт
+        shared_mem_ptr[i] = fgetc(file); // считываем с помощью fgetc, но можно читать и read`ом "блоками" по sizeof(char) байт
         
     fclose(file);
+    // детатчим разделяемую память для данного процесса
+    if (shmdt(shared_mem_ptr_size) < 0) {
+        printf("Can't detach shared memory\n");
+        exit(-1);
+    }
     // детатчим разделяемую память для данного процесса
     if (shmdt(shared_mem_ptr) < 0) {
         printf("Can't detach shared memory\n");
--- src/lab7_2.c
+++ src/lab7_2.c
@@ -8,37 +8,67 @@
 
 int main() {
     int *array;
-    int shmid;
+    int shmid_for_size, shmid;
     char pathname[] = "lab7_1.c";
-    key_t key;
-    char *source_code;
+    char pathname_for_size[] = "lab7_2.c";
+    key_t key_for_size, key;
     
     int file_size = 0;
-    int *shared_mem_ptr;
+    int *shared_mem_ptr_size;
+    char *shared_mem_ptr;
+
+    // генерируем ключ разделяемой памяти для переменной размера файла
+    if ((key_for_size = ftok(pathname_for_size, 0)) < 0) { 
+        printf("Can\'t generate key\n"); 
+        exit(-1);
+    }
     // генерируем ключ разделяемой памяти (позволяет нам не пересекать деятельность независимых процессов)
     if ((key = ftok(pathname, 0)) < 0) {
         printf("Can\'t generate key\n");
         exit(-1);
     }
-    // получаем по ключу дескриптор нужного размера, в этот раз в флаги передаем 0, ибо память уже создана
-    if ((shmid = shmget(key, sizeof(int) + file_size * sizeof(char), 0)) < 0) {
-        printf("Can\'t create shared memory\n");
+
+    // получаем по ключу дескриптор размера в один инт
+    if ((shmid_for_size = shmget(key_for_size, sizeof(int), 0)) < 0) {
+        printf("Can\'t create shared memory!\n");
         exit(-1);
     }
     // получаем адрес нашей разделяемой памяти для работы с ней
-    if ((shared_mem_ptr = (int *)shmat(shmid, 
+    if ((shared_mem_ptr_size = (int *)shmat(shmid_for_size, 
                                         NULL, // желаемый адрес, куда мы хотим записать нашу разделяемую память, в нашем случае нам все равно
                                         0)) == (int *)(-1)) {
         printf("Can't attach shared memory\n");
         exit(-1);
     }
-    // нагло крадем с первого слота разделяемой памяти размер файла для чтения (ибо мы также нагло его туда записали)
-    file_size = *shared_mem_ptr;
-    // двигаем наш указатель на один инт, ибо там хранится размер файла, и приводим к чару для верной дальнейшей итерации
-    source_code = (char*)(shared_mem_ptr + 1);
-    // теперь итерируемся по оставшейся памяти и считываем сам файл 
+    // нагло крадем из разделяемой памяти размер файла для чтения (ибо мы также нагло его туда записали)
+    file_size = *shared_mem_ptr_size;
+    // детатчим разделяемую память для данного процесса
+    if (shmdt(shared_mem_ptr_size) < 0) {
+        printf("Can't detach shared memory\n");
+        exit(-1);
+    }
+    // удаляем разделяемую память
+    if (shmctl(shmid_for_size, IPC_RMID, NULL) < 0) {
+        printf("Can't delete shared memory\n");
+        exit(-1);
+    }
+
+    // получаем по ключу дескриптор нужного размера, в этот раз в флаги передаем 0, ибо память уже создана
+    if ((shmid = shmget(key, file_size * sizeof(char), 0)) < 0) {
+        printf("Can\'t create shared memory\n");
+        exit(-1);
+    }
+    // получаем адрес нашей разделяемой памяти для работы с ней
+    if ((shared_mem_ptr = (char *)shmat(shmid, 
+                                        NULL, // желаемый адрес, куда мы хотим записать нашу разделяемую память, в нашем случае нам все равно
+                                        0)) == (char *)(-1)) {
+        printf("Can't attach shared memory\n");
+        exit(-1);
+    }
+
+    // теперь итерируемся по памяти и считываем сам файл 
     for (int i = 0; i < file_size; i++)
-        printf("%c", source_code[i]);
+        printf("%c", shared_mem_ptr[i]);
     // детатчим разделяемую память для данного процесса
     if (shmdt(shared_mem_ptr) < 0) {
         printf("Can't detach shared memory\n");
